892fbbdba46878e4eea692f301702ac03bd5ae83
Hadi <hadilearn01@gmail.com>
2021-08-05 16:46:12 +0700

    day 1 project finished

---
+++ day-1-working-with-variables/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+input_city = input("What's name of the city you grew up in?\n")
+input_pet = input("What's your pet's name?\n")
+print("Your band name could be " + input_city + " " + input_pet)
