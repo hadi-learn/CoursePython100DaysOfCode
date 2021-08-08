df674a20e732860d417e05ed4f0933bac50bbd4a
Hadi <hadilearn01@gmail.com>
2021-08-08 14:40:49 +0700

    day 57 optimizing code using for loop

--- day-57-blog-templating-start/main.py
+++ day-57-blog-templating-start/main.py
@@ -1,23 +1,26 @@
 from flask import Flask, render_template
 import requests
+from post import Post
 from datetime import datetime
 
 current_year = datetime.now().year
+all_objects = []
+blog_url = "https://api.npoint.io/4358ffbd346683abf0e4"
+all_posts = requests.get(blog_url).json()
+for post in all_posts:
+    post_object = Post(post_id=post["id"], post_title=post["title"], post_subtitle=post["subtitle"], post_body=post["body"])
+    all_objects.append(post_object)
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    blog_url = "https://api.npoint.io/4358ffbd346683abf0e4"
-    all_posts = requests.get(blog_url).json()
-    return render_template("index.html", posts=all_posts, year=current_year)
+    return render_template("index.html", posts=all_objects, year=current_year)
 
 
 @app.route("/post/<int:num>")
 def to_post(num):
-    print(num)
-    blog_url = "https://api.npoint.io/4358ffbd346683abf0e4"
-    all_posts = requests.get(blog_url).json()
     return render_template("post.html", posts=all_posts, num=num, year=current_year)
 
 if __name__ == "__main__":
--- day-57-blog-templating-start/post.py
+++ day-57-blog-templating-start/post.py
@@ -1,2 +1,6 @@
 class Post:
-    pass
+    def __init__(self, post_id, post_title, post_subtitle, post_body):
+        self.id = post_id
+        self.title = post_title
+        self.subtitle = post_subtitle
+        self.body = post_body
