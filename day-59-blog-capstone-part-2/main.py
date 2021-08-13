f9f247ffb00f71168c90ebd4744a3a30c4fdce04
Hadi <hadilearn01@gmail.com>
2021-08-13 08:34:17 +0700

    day 59 blog capstone project finished

--- day-59-blog-capstone-part-2/main.py
+++ day-59-blog-capstone-part-2/main.py
@@ -1,14 +1,30 @@
 from flask import Flask, render_template
 import requests
 from datetime import datetime
+import calendar
 
 current_year = datetime.now().year
+all_objects = []
+all_date = []
+all_body = []
+total = len(all_objects)
+blog_url = "https://api.npoint.io/4358ffbd346683abf0e4"
+all_posts = requests.get(blog_url).json()
+for post in all_posts:
+    all_objects.append(post)
+    day = post["date"].split("-")[0]
+    month = calendar.month_name[int(post["date"].split("-")[1])]
+    year = post["date"].split("-")[2]
+    date = (day, month, year)
+    all_date.append(date)
+    sentences = [sentence.replace(".", "") for sentence in post["body"].split(". ")]
+    all_body.append(sentences)
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", posts=all_objects, date=all_date)
 
 @app.route("/about")
 def about():
@@ -16,7 +32,12 @@ def about():
 
 @app.route("/contact")
 def contact():
-    return render_template("/contact.html")
+    return render_template("contact.html")
+
+@app.route("/post/<int:num>")
+def to_post(num):
+    return render_template("post.html", posts=all_objects, num=num, date=all_date, bodies=all_body)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
